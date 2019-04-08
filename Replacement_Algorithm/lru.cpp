64fa808786f7a0cdfc9e548f837646d21cc684c9
vologue <raghav.sp98@gmail.com>
2019-04-08 22:05:13 +0530

    Lru

--- Replacement_Algorithm/lru.cpp
+++ Replacement_Algorithm/lru.cpp
@@ -4,27 +4,35 @@
 using namespace std;
 
 struct page{
+    int time;
     int num;
 };
 
-bool checkin(page *ar,int n,int num){     //Returns 1 if the page already is in the memory
+bool checkin(page *ar,int n,int num,int m){     //Returns 1 if the page already is in the memory
     for(int i=0;i<n;i++){
         if(num==ar[i].num){
+            ar[i].time=m+1;
             return 1;
         }
     }
     return 0;
 }
 
+bool comp(page a, page b) { 
+    return (a.time < b.time); 
+} 
+
 int main(){
     int a[]={1,2,3,4,5,6,3,2,5,2};                              //take input in array a and size in insize
     int n,insize=10,page_faults=0;                              //number of pages we can store at one time
     cin>>n;
-    page ar[10]={0};
-
+    page ar[10]={{-1,0}};
     for(int i=0; i<insize; i++){                  
-        if(!checkin(ar,n,a[i])){
-            ar[page_faults%n].num=a[i];                   //Replacement
+        if(!checkin(ar,n,a[i],i)){
+            page *m;
+            m= min_element(ar,ar+n,comp);
+            m->num=a[i];
+            m->time=i+1;
             page_faults++;
         }
     }
